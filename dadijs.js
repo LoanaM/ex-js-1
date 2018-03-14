ed8017502983de0a63ee3e9380e92d6f039a7c6a
LoanaWitch <loanawitch@live.com>
2018-03-14 20:05:36 +0100

    dadi done

---
+++ dadijs.js
@@ -0,0 +1,16 @@
+var player1 = Math.floor((Math.random() * 6) + 1);
+var player2 = Math.floor((Math.random() * 6) + 1);
+var gioc1 = 1
+var gioc2 = 5
+
+document.write (player1);
+document.write (player2);
+
+    if (player1 > player2) {
+        result = "Il giocatore1 ha vinto!";
+    } else if (player1 == player2) {
+        result = "Pareggio!";
+    } else {
+        result = "Il giocatore2 ha vinto!";
+    }
+document.write (result);
